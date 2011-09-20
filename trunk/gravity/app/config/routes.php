bdf5431f321738f5f1eb48d62c58e3c5f0d3f145
michael@jloop.com <michael@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2011-09-20 21:11:22 +0000

    forum

--- trunk/gravity/app/config/routes.php
+++ trunk/gravity/app/config/routes.php
@@ -31,3 +31,4 @@
  * ...and connect the rest of 'Pages' controller's urls.
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+	Router::connect('/forum', array('plugin' => 'forum', 'controller' => 'home', 'action' => 'index'));
