7de9b92605bb53d5a8a4da5ba52b21b7c5019151
michael <michael@jloop.com>
2011-10-11 19:45:06 +0000

    Alerts

--- gravity/app/webroot/movement.php
+++ gravity/app/webroot/movement.php
@@ -66,7 +66,7 @@
 		
 		var target = new Object;
 		var player = new Object;
-		player.x = player.y = target.x = target.y = 0;
+		player.x = player.y = target.x = target.y = 200;
 		player.w = player.h = 16;
 		player.xOffset = 8;
 		player.yOffset = 12;
