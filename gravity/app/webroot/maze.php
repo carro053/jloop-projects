9dfdd80d9523afdeb276520fd6572bc5b7a62224
michael <michael@jloop.com>
2012-02-06 23:53:00 +0000

    Artists

--- gravity/app/webroot/maze.php
+++ gravity/app/webroot/maze.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="/home_files/js/jquery-1.6.4.min.js"></script>
+<script type="text/javascript" src="/js/jquery-1.6.2.min.js"></script>
 <?php
 srand(ceil(time()/300));
 function setAsMaze($x,$y)
