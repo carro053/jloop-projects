96d7fccd854ff69190a40eb5a14961b2c93711e4
michael <michael@jloop.com>
2012-03-20 23:26:28 +0000

    fixing bug

--- do-we-have-enough/web/trunk/app/controllers/users_controller.php
+++ do-we-have-enough/web/trunk/app/controllers/users_controller.php
@@ -43,6 +43,7 @@ class UsersController extends AppController
 	
 	function event_list()
 	{
+		set_time_limit(0);
 		Configure::write('debug', '2');
 		$this->layout = 'dowehaveenough_main';
 		$this->set('data',$this->Event->findAll(null,null,'Event.created DESC',20));
