1c878a97ae0d063c3dd64a7da95860c7c2de1027
michael <michael@jloop.com>
2012-04-16 22:34:12 +0000

    Posting to server testing

--- gravity/app/controllers/puzzles_controller.php
+++ gravity/app/controllers/puzzles_controller.php
@@ -15,16 +15,16 @@ class PuzzlesController extends AppController {
  	function savePuzzle()
  	{
  		$json_data = json_decode($_POST['json_data']);
- 		CakeLog::write('savePuzzle',print_r($json_data,true).' '.$json_data->total_fuel.' '.$json_data->puzzle_id);
+ 		CakeLog::write('savePuzzle',print_r($json_data,true).' '.$json_data->total_fuel.' '.$json_data->server_id);
  		$puzzle['Puzzle']['account_id'] = 1;
  		$puzzle['Puzzle']['total_fuel'] = $json_data->total_fuel;
  		$puzzle['Puzzle']['start_x'] = 1;
  		$puzzle['Puzzle']['start_y'] = 1;
  		$puzzle['Puzzle']['end_x'] = 1;
  		$puzzle['Puzzle']['end_y'] = 1;
- 		if($json_data->puzzle_id > 0)
+ 		if($json_data->server_id > 0)
  		{
- 			$puzzle['Puzzle']['id'] = $json_data['puzzle_id'];
+ 			$puzzle['Puzzle']['id'] = $json_data->server_id;
  			$this->Puzzle->save($puzzle);
  			$puzzle_id = $puzzle['Puzzle']['id'];
  			$this->PuzzlePlanet->query('DELETE FROM `puzzle_planets` WHERE `puzzle_id` = '.$puzzle_id);
@@ -33,7 +33,7 @@ class PuzzlesController extends AppController {
  			$this->Puzzle->save($puzzle);
  			$puzzle_id = $this->Puzzle->getLastInsertId();
  		}
- 		foreach($json_data['planets'] as $planet)
+ 		foreach($json_data->planets as $planet)
  		{
  			$newplanet['PuzzlePlanet']['puzzle_id'] = $puzzle_id;
  			$newplanet['PuzzlePlanet']['x'] = $planet->x;
@@ -42,13 +42,15 @@ class PuzzlesController extends AppController {
  			$newplanet['PuzzlePlanet']['density'] = $planet->density;
  			$newplanet['PuzzlePlanet']['hasMoon'] = $planet->hasMoon;
  			$newplanet['PuzzlePlanet']['moonAngle'] = $planet->moonAngle;
+ 			CakeLog::write('savePuzzle',print_r($newplanet,true));
  			$this->PuzzlePlanet->save($newplanet);
  		}
- 		foreach($json_data['astronauts'] as $astronaut)
+ 		foreach($json_data->astronauts as $astronaut)
  		{
  			$newastro['PuzzlePlanet']['puzzle_id'] = $puzzle_id;
  			$newastro['PuzzlePlanet']['x'] = $astronaut->x;
  			$newastro['PuzzlePlanet']['y'] = $astronaut->y;
+ 			CakeLog::write('savePuzzle',print_r($newastro,true));
  			$this->PuzzleAstronaut->save($newastro);
  		}
  		
