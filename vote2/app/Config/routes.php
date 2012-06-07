fd69a22f867289591e0502c62c8f1f74bdbaa5e5
todd <todd@jloop.com>
2012-06-07 21:28:26 +0000

    vote 2

--- vote2/app/Config/routes.php
+++ vote2/app/Config/routes.php
@@ -25,11 +25,16 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	//Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+/**
+ * ...custom.
+ */
+	Router::connect('/', array('controller' => 'questions', 'action' => 'index'));
+
 
 /**
  * Load all plugin routes.  See the CakePlugin documentation on 
