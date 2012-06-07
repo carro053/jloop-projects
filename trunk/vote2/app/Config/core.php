1c14543408d9a82a6fcb76323ca645959c917680
todd@jloop.com <todd@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2012-06-07 18:28:20 +0000

    vote 2

--- trunk/vote2/app/Config/core.php
+++ trunk/vote2/app/Config/core.php
@@ -184,12 +184,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'BbaGf8c26f6Bb908LeFceafCb581e754Nd5cM012');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '019375648303947918749865781786098');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
