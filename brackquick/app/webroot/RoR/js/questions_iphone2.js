494ae52d49c9b4e547145e8cf68da70871e3e720
todd <todd@jloop.com>
2012-06-22 01:00:00 +0000

    5-5

---
+++ brackquick/app/webroot/RoR/js/questions_iphone2.js
@@ -0,0 +1,64 @@
+var questions = [
+	{
+		title: 'Unplayed Question 1',
+		time: 'Today, 12:00pm',
+		question_type: 'text',
+		answer_type: 'text',
+		clue_type: 'text',
+		insight_type: 'text',
+		category: 'NERC-CIP',
+		learn_more_url: '',
+		clue: 'From personal to digital',
+		question: 'What does NERC CIP Standard CIP-005 focus on? ',
+		answers: [
+			'Electronic Security Perimeter(s)',
+			'Physical Security of CCAs',
+			'Personnel & Training',
+			'Freudian issues of hackers'
+			],
+		correct_answer_index: 0,
+		insight: 'Where CIP 4 focused on "human" processes, CIP 5 security focuses on electronic processes.'
+	},
+	
+	{
+		title: 'Unplayed Question 2',
+		time: 'Today, 12:00pm',
+		question_type: 'text',
+		answer_type: 'text',
+		clue_type: 'text',
+		insight_type: 'text',
+		category: 'NERC-CIP',
+		learn_more_url: '',
+		clue: 'Keep it lean',
+		question: 'What must be done with ports and services not used for normal operations or emergencies?',
+		answers: [
+			'Monitor them',
+			'Disable them',
+			'Reroute them',
+			'Show them to friends'
+			],
+		correct_answer_index: 1,
+		insight: 'Remember, if it can\'t be disabled, document a compensating measure to mitigate risk.'
+	},
+	
+	{
+		title: 'Unplayed Question 3',
+		time: 'Today, 12:00pm',
+		question_type: 'text',
+		answer_type: 'text',
+		clue_type: 'text',
+		insight_type: 'text',
+		category: 'NERC-CIP',
+		learn_more_url: '',
+		clue: 'The alarm goes off at the gate...',
+		question: 'Unauthorized physical access attempts must be reviewed when?',
+		answers: [
+			'At least annually',
+			'At least every 30 days',
+			'Immediately',
+			'at least before you\'re audited'
+			],
+		correct_answer_index: 2,
+		insight: 'Handle such incidences according to procedures in Requirement CIP-008-4'
+	}
+];
