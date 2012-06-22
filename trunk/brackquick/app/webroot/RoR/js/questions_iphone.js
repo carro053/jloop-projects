477b382d2087c7020a5d0214acbb8389629aa5b6
chris@jloop.com <chris@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2012-06-22 20:59:23 +0000

    learn more URL

--- trunk/brackquick/app/webroot/RoR/js/questions_iphone.js
+++ trunk/brackquick/app/webroot/RoR/js/questions_iphone.js
@@ -1,3 +1,6 @@
+
+
+
 var questions = [
 	{
 		title: 'Unplayed Question 1',
@@ -7,16 +10,17 @@ var questions = [
 		clue_type: 'text',
 		insight_type: 'text',
 		category: 'Air Leaks',
+		learn_more_title: '',
 		learn_more_url: '',
 		clue: 'John from N. Las Vegas is sweating his high power bill in summer, and draftiness in winter. So he\'s headed to the home improvement store...',
 		question: 'Which of these is an easy-to-install, inexpensive money saver that keeps the heat and cold where they belong?',
 		answers: [
-			'11-1-514.png',
-			'11-2-514.png',
-			'11-3-514.png',
-			'11-4-514.png'
+			'11-1-563.png',
+			'11-2-563.png',
+			'11-3-563.png',
+			'11-4-563.png'
 			],
-		correct_answer_index: 1,
+		correct_answer_index: 2,
 		insight: 'Door-sweeps screw in easily to the bottom of the door.  In summer they keep John\'s cool air in, warm air out and money in his pocket.'
 	},
 	
@@ -28,7 +32,8 @@ var questions = [
 		clue_type: 'text',
 		insight_type: 'text',
 		category: 'Air Leaks',
-		learn_more_url: 'http://www.youtube.com/watch?v=Kz5obdcraY4',
+		learn_more_title: '',
+		learn_more_url: '',
 		clue: 'Only 15 percent of air leakage is through windows and doors',
 		question: 'Which of these sealing actions can lower your bill, even more so than replacing windows?',
 		answers: [
@@ -38,7 +43,7 @@ var questions = [
 			'Wearing full-body spandex'
 			],
 		correct_answer_index: 0,
-		insight: 'Caulking the attic and foundation is often neglected as an impactful money saver.  Check and don\'t neglect: http://www.youtube.comwatch?v=Kz5obdcraY4'
+		insight: 'Caulking the attic and foundation is often neglected as an impactful money saver.  Check and don\'t neglect (follow the "learn more" link).'
 	},
 	
 	{
@@ -49,6 +54,7 @@ var questions = [
 		clue_type: 'text',
 		insight_type: 'image',
 		category: 'Air Leaks',
+		learn_more_title: '',
 		learn_more_url: '',
 		clue: 'Adele from Ely is dedicated to saving money by sealing up air leaks in the house. But her baby daughter reminded her of a crucial step she missed!',
 		question: 'True or False? Using child safety plugs with an insulated backing can lower energy costs by preventing air leakage.',
@@ -70,8 +76,8 @@ var questions = [
 		clue_type: 'text',
 		insight_type: 'text',
 		category: 'Air Leaks',
-		learn_more_url: 'http://www.energystar.gov/index.cfm?c=diy.diy_index',
-		learn_more_title: 'Likely Source for Energy Loss',
+		learn_more_title: '',
+		learn_more_url: '',
 		clue: 'Air is money',
 		question: 'Which of these pictures shows money being wasted?',
 		answers: [
@@ -92,6 +98,7 @@ var questions = [
 		clue_type: 'text',
 		insight_type: 'text',
 		category: 'Air Leaks',
+		learn_more_title: '',
 		learn_more_url: '',
 		clue: 'William from Wells stands ready to defend his home from intruders.  That includes cold air drafts.',
 		question: 'When you find poor sealing or insulation, what is the correct order for fixing it?',
@@ -113,7 +120,8 @@ var questions = [
 		clue_type: 'text',
 		insight_type: 'text',
 		category: 'Air Leaks',
-		learn_more_url: 'http://www.energyefficientsolutions.com/smoke_pen.asp',
+		learn_more_title: '',
+		learn_more_url: '',
 		clue: 'James from Fernley learned an inexpensive trick from his brother on how to detect a leak, while keeping things fragrant...',
 		question: 'Is an incense stick an effective and inexpensive tool to save energy?',
 		answers: [
@@ -123,7 +131,7 @@ var questions = [
 			'It\'s groovy.  Just groovy.'
 			],
 		correct_answer_index: 1,
-		insight: 'When James holds up the lit incense, the smoke moves horizontally instead of upward, indicating an airleak that needs sealing.  Click "learn more" for an alternative.'
+		insight: 'When James holds up the lit incense, the smoke moves horizontally instead of upward, indicating an air leak that needs sealing.  Click "learn more" for an alternative.'
 	},
 	
 	{
@@ -134,6 +142,7 @@ var questions = [
 		clue_type: 'text',
 		insight_type: 'text',
 		category: 'Air Leaks',
+		learn_more_title: '',
 		learn_more_url: '',
 		clue: 'Teresa from Fallon was concerned when her daughter first suggested she would be getting some vinyl for stripping -- until she pointed to the windows and doors.',
 		question: 'Which material is NOT appropriate for weather stripping?',
@@ -155,6 +164,7 @@ var questions = [
 		clue_type: 'text',
 		insight_type: 'text',
 		category: 'Air Leaks',
+		learn_more_title: '',
 		learn_more_url: '',
 		clue: 'Steve from Sparks read that he would save money on his electric bill if he caulked and weatherstripped. So he weatherstripped his front door. And then caulked it... uh...',
 		question: 'Where is it best to use weatherstripping instead of caulk?',
@@ -178,6 +188,7 @@ var questions = [
 		clue_type: 'text',
 		insight_type: 'text',
 		category: 'Air Leaks',
+		learn_more_title: '',
 		learn_more_url: '',
 		clue: 'Maria in Winnemucca has the misfortune of sleeping in the coldest room in the house.',
 		question: 'If one room in your house is always colder or hotter than others, what\'s the most likely cause?',
@@ -191,191 +202,3 @@ var questions = [
 		insight: 'Maria is not going to take it anymore. She is joining her neighbors in hiring a professional to inspect and repair ductwork.'
 	}
 ];
-
-/*
-var questions = [
-	{
-		title: 'Unplayed Question 1',
-		time: '12/03/12 10:16am',
-		question_type: 'text',
-		answer_type: 'text',
-		clue_type: 'text',
-		insight_type: 'text',
-		category: 'Air Leaks',
-		clue: 'It sweeps without a broom.',
-		question: 'What product is easy to install, saves energy and lowers utility bills?',
-		answers: [
-			'Fan',
-			'Door-sweep',
-			'Window tint',
-			'Swamp cooler'
-		],
-		correct_answer_index: 1,
-		insight: 'Door-sweeps can be found at any home improvement store and keep cool air in and warm air out. They easily screw into the bottom of any door.'
-	},
-	
-	{
-		title: 'Unplayed Question 2',
-		time: '12/03/12 11:42am',
-		question_type: 'text',
-		answer_type: 'text',
-		clue_type: 'text',
-		insight_type: 'text',
-		category: 'Air Leaks',
-		clue: 'Only 15 percent of air leakage is through windows and doors.',
-		question: 'Replacing windows is the <u>only</u> way to stop air leaks.',
-		answers: [
-			null,
-			null,
-			'True',
-			'False'
-		],
-		correct_answer_index: 3,
-		insight: 'Caulking the attic and foundation will lower your  cost, oftentimes more than replacing windows.'
-	},
-	{
-		title: 'Unplayed Question 3',
-		time: '12/03/12 1:33pm',
-		question_type: 'text',
-		answer_type: 'text',
-		clue_type: 'image',
-		insight_type: 'text',
-		category: 'Air Leaks',
-		clue: 'child_safety_plugs.png',
-		question: 'Using child safety plugs with an insulated backing can lower energy costs by preventing air leakage.',
-		answers: [
-			null,
-			null,
-			'True',
-			'False'
-			],
-		correct_answer_index: 2,
-		insight: 'Plugging small air leaks can add up to big savings in utility bills. '
-	},
-	{
-		title: 'Unplayed Question 4',
-		time: '12/03/12 2:47pm',
-		question_type: 'text',
-		answer_type: 'image',
-		clue_type: 'text',
-		insight_type: 'text',
-		category: 'Air Leaks',
-		clue: 'Have you noticed these common areas that leak air?',
-		question: 'Which of the following are likely to be a source for energy loss (and higher utility bills):',
-		answers: [
-			'cable_tv_line.png',
-			'dryer_vent.png',
-			'mail_slot.png',
-			'all_of_the_above.png'
-			],
-		correct_answer_index: 3,
-		insight: 'Plugging small air leaks can add up to big savings in utility bills. '
-	},
-	{
-		title: 'Unplayed Question 5',
-		time: '12/03/12 2:47pm',
-		question_type: 'text',
-		answer_type: 'text',
-		clue_type: 'text',
-		insight_type: 'text',
-		category: 'Air Leaks',
-		clue: 'Start with the seal.',
-		question: 'The correct order when sealing and insulating is: ',
-		answers: [
-			'Seal before insulating.',
-			'Insulate before sealing.',
-			'Insulating eliminates the need for sealing.',
-			'None of the above.'
-			],
-		correct_answer_index: 0,
-		insight: 'It\'s important to seal leaks first. Insulating materials don\'t block leaks.'	
-	},
-	{
-		title: 'Unplayed Question 6',
-		time: '12/03/12 2:47pm',
-		question_type: 'text',
-		answer_type: 'text',
-		clue_type: 'text',
-		insight_type: 'text',
-		category: 'Air Leaks',
-		clue: 'Stick to the task.',
-		question: 'Using an incense stick or smoke pen is an inexpensive way to check for air leaks. ',
-		answers: [
-			null,
-			null,
-			'True',
-			'False'
-			],
-		correct_answer_index: 2,
-		insight: 'If the smoke moves horizontally instead of upward, you have an air leak that might need to be sealed.'
-	},
-	
-	
-	{
-		title: 'Unplayed Question 7',
-		time: '12/03/12 2:47pm',
-		question_type: 'text',
-		answer_type: 'text',
-		clue_type: 'text',
-		insight_type: 'text',
-		category: 'Air Leaks',
-		clue: 'Vinyl shines when it comes to weather-stripping.',
-		question: 'Which weather-stripping material is considered the most durable and weather-resistant:',
-		answers: [
-			'Felt or foam',
-			'Vinyl',
-			'Metal',
-			'All of the above.'
-			],
-		correct_answer_index: 1,
-		insight: 'Felt and foam are inexpensive but vinyl is more durable and resistant to weather.'
-	},
-	
-	{
-		title: 'Unplayed Question 8',
-		time: '12/03/12 2:47pm',
-		question_type: 'text',
-		answer_type: 'text',
-		clue_type: 'text',
-		insight_type: 'text',
-		category: 'Air Leaks',
-		clue: 'If it moves, use weatherstripping.',
-		question: 'Where is it best to use weatherstripping instead of caulk?',
-		answers: [
-			'Around baseboards.',
-			'Around windows and doors.',
-			'Around recessed lights.',
-			'Around outlets and switches.'
-			],		correct_answer_index: 1,
-		insight: 'Weatherstripping is best around moveable joints. (Be sure to apply it in a way that allows the window or door to operate freely.).',
-		prize: 'giftcard-36.png'
-	},
-	
-	//////PRIZE
-	{
-		title: 'Unplayed Question 9',
-		time: '12/03/12 2:47pm',
-		question_type: 'text',
-		answer_type: 'text',
-		clue_type: 'text',
-		insight_type: 'text',
-		category: 'Air Leaks',
-		clue: 'Leaks are a likely culprit.',
-		question: 'One room that is more difficult to heat or cool than others could be a sign that:',
-		answers: [
-			'The furnace needs replacing.',
-			'The air conditioner needs replacing.',
-			'The filter needs to be changed.',
-			'The ductwork might be leaky.'
-			],
-		correct_answer_index: 3,
-		insight: 'Many homeowners choose to hire a professional to inspect and repair ductwork.'
-	}
-	
-
-	
-
-		
-];
-
-*/
