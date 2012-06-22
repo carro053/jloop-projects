b699a92f20fbefaaa513db50f826dfc5e85e66f2
chris@jloop.com <chris@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2012-06-22 00:12:48 +0000

    learn more URL

--- trunk/brackquick/app/webroot/RoR/js/questions_iphone.js
+++ trunk/brackquick/app/webroot/RoR/js/questions_iphone.js
@@ -1,6 +1,200 @@
 var questions = [
 	{
 		title: 'Unplayed Question 1',
+		time: 'Today, 12:00pm',
+		question_type: 'text',
+		answer_type: 'image',
+		clue_type: 'text',
+		insight_type: 'text',
+		category: 'Air Leaks',
+		learn_more_url: '',
+		clue: 'John from N. Las Vegas is sweating his high power bill in summer, and draftiness in winter. So he\'s headed to the home improvement store...',
+		question: 'Which of these is an easy-to-install, inexpensive money saver that keeps the heat and cold where they belong?',
+		answers: [
+			'11-1-514.png',
+			'11-2-514.png',
+			'11-3-514.png',
+			'11-4-514.png'
+			],
+		correct_answer_index: 1,
+		insight: 'Door-sweeps screw in easily to the bottom of the door.  In summer they keep John\'s cool air in, warm air out and money in his pocket.'
+	},
+	
+	{
+		title: 'Unplayed Question 2',
+		time: 'Today, 12:00pm',
+		question_type: 'text',
+		answer_type: 'text',
+		clue_type: 'text',
+		insight_type: 'text',
+		category: 'Air Leaks',
+		learn_more_url: '',
+		clue: 'Only 15 percent of air leakage is through windows and doors',
+		question: 'Which of these sealing actions can lower your bill, even more so than replacing windows?',
+		answers: [
+			'Caulking the attic',
+			'Replacing the carpet',
+			'Turning off the oven',
+			'Wearing full-body spandex'
+			],
+		correct_answer_index: 0,
+		insight: 'Caulking the attic and foundation is often neglected as an impactful money saver.  Check and don\'t neglect: http://www.youtube.comwatch?v=Kz5obdcraY4'
+	},
+	
+	{
+		title: 'Unplayed Question 3',
+		time: 'Today, 12:00pm',
+		question_type: 'text',
+		answer_type: 'text',
+		clue_type: 'text',
+		insight_type: 'image',
+		category: 'Air Leaks',
+		learn_more_url: '',
+		clue: 'Adele from Ely is dedicated to saving money by sealing up air leaks in the house. But her baby daughter reminded her of a crucial step she missed!',
+		question: 'True or False? Using child safety plugs with an insulated backing can lower energy costs by preventing air leakage.',
+		answers: [
+			null,
+			null,
+			'True',
+			'False'
+			],
+		correct_answer_index: 2,
+		insight: '13-513.png'
+	},
+	
+	{
+		title: 'Unplayed Question 4',
+		time: 'Today, 12:00pm',
+		question_type: 'text',
+		answer_type: 'image',
+		clue_type: 'text',
+		insight_type: 'text',
+		category: 'Air Leaks',
+		learn_more_url: '',
+		clue: 'Air is money',
+		question: 'Which of these pictures shows money being wasted?',
+		answers: [
+			'14-1-463.png',
+			'14-2-463.png',
+			'14-3-463.png',
+			'14-4-463.png'
+			],
+		correct_answer_index: 3,
+		insight: 'Air leaks in your house are no different than dollars leaking out of your wallet.  See how simple it is to check for leaks and change, by following the "learn more" link.'
+	},
+	
+	{
+		title: 'Unplayed Question 5',
+		time: 'Today, 12:00pm',
+		question_type: 'text',
+		answer_type: 'text',
+		clue_type: 'text',
+		insight_type: 'text',
+		category: 'Air Leaks',
+		learn_more_url: '',
+		clue: 'William from Wells stands ready to defend his home from intruders.  That includes cold air drafts.',
+		question: 'When you find poor sealing or insulation, what is the correct order for fixing it?',
+		answers: [
+			'Seal before insulating.',
+			'Insulate before sealing.',
+			'One or the other, but not both.',
+			'Cry.  Then laugh.  Then eat.'
+			],
+		correct_answer_index: 0,
+		insight: 'William routinely checks for leaks when he cleans the house. Sealing and insulating are both important. But sealing FIRST helps to block leaks.'
+	},
+	
+	{
+		title: 'Unplayed Question 6',
+		time: 'Today, 12:00pm',
+		question_type: 'text',
+		answer_type: 'text',
+		clue_type: 'text',
+		insight_type: 'text',
+		category: 'Air Leaks',
+		learn_more_url: '',
+		clue: 'James from Fernley learned an inexpensive trick from his brother on how to detect a leak, while keeping things fragrant...',
+		question: 'Is an incense stick an effective and inexpensive tool to save energy?',
+		answers: [
+			'It\'s inexpensive but ineffective',
+			'Yes its smoke can indicate an air leak',
+			'No it is relatively expensive',
+			'It\'s groovy.  Just groovy.'
+			],
+		correct_answer_index: 1,
+		insight: 'When James holds up the lit incense, the smoke moves horizontally instead of upward, indicating an airleak that needs sealing.  Click "learn more" for an alternative.'
+	},
+	
+	{
+		title: 'Unplayed Question 7',
+		time: 'Today, 12:00pm',
+		question_type: 'text',
+		answer_type: 'text',
+		clue_type: 'text',
+		insight_type: 'text',
+		category: 'Air Leaks',
+		learn_more_url: '',
+		clue: 'Teresa from Fallon was concerned when her daughter first suggested she would be getting some vinyl for stripping -- until she pointed to the windows and doors.',
+		question: 'Which material is NOT appropriate for weather stripping?',
+		answers: [
+			'Copper',
+			'Felt',
+			'Foam',
+			'Vinyl'
+			],
+		correct_answer_index: 0,
+		insight: 'Teresa asks her husband to apply the most durable stripping, vinyl, to their house windows.  Their daughter is on a budget, so she uses foam on her apartment windows.'
+	},
+	
+	{
+		title: 'Unplayed Question 8',
+		time: 'Today, 12:00pm',
+		question_type: 'text',
+		answer_type: 'text',
+		clue_type: 'text',
+		insight_type: 'text',
+		category: 'Air Leaks',
+		learn_more_url: '',
+		clue: 'Steve from Sparks read that he would save money on his electric bill if he caulked and weatherstripped. So he weatherstripped his front door. And then caulked it... uh...',
+		question: 'Where is it best to use weatherstripping instead of caulk?',
+		answers: [
+			'Around baseboards',
+			'Around windows and doors',
+			'Around recessed lights',
+			'Around friends'
+			],
+		correct_answer_index: 1,
+		insight: 'Friends don\'t let friends caulk themselves into their own home. Steve may not be the sharpest tool in the shed, but he\'ll certainly save on his bill!',
+		prize: '18-426.png',
+		prize_text: 'You have been selected to receive a <strong>$25.00 Home Depot Gift Card</strong> for playing questions every day this week.'
+	},
+	
+	{
+		title: 'Unplayed Question 9',
+		time: 'Today, 12:00pm',
+		question_type: 'text',
+		answer_type: 'text',
+		clue_type: 'text',
+		insight_type: 'text',
+		category: 'Air Leaks',
+		learn_more_url: '',
+		clue: 'Maria in Winnemucca has the misfortune of sleeping in the coldest room in the house.',
+		question: 'If one room in your house is always colder or hotter than others, what\'s the most likely cause?',
+		answers: [
+			'frayed carpet',
+			'eroding walls',
+			'leaky ductwork',
+			'menopause'
+			],
+		correct_answer_index: 2,
+		insight: 'Maria is not going to take it anymore. She is joining her neighbors in hiring a professional to inspect and repair ductwork.'
+	}
+];
+
+/*
+var questions = [
+	{
+		title: 'Unplayed Question 1',
 		time: '12/03/12 10:16am',
 		question_type: 'text',
 		answer_type: 'text',
@@ -182,3 +376,5 @@ var questions = [
 
 		
 ];
+
+*/
