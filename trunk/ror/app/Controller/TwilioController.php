4c805e5a30141c93daba975393366dc2deb4bf80
todd@jloop.com <todd@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2012-07-02 18:02:48 +0000

    ror twilio

--- trunk/ror/app/Controller/TwilioController.php
+++ trunk/ror/app/Controller/TwilioController.php
@@ -13,4 +13,28 @@ class TwilioController extends AppController {
 		echo 123;
 		die;
 	}
+	
+	public function sendTest() {
+	
+		$AccountSid = "AC381c7e26c9a5de66108a8fd7f46f841a";
+		$AuthToken = "2616c3dda6897aa8f1dc3c0f346f18b1";
+		
+		$client = new Services_Twilio($AccountSid, $AuthToken);
+		
+		$people = array(
+			//"+18183899197" => "Chris",
+			"+13105031577" => "Todd",
+			//"+14246341622" => "Mike",
+		);
+		
+		foreach ($people as $number => $name) {
+			$sms = $client->account->sms_messages->create(
+				"+14155992671",
+				$number,
+				"Hey $name, Monkey Party at 6PM. Bring Bananas!"
+			);
+			
+			echo "Sent message to $name";
+		}
+	}
 }
