f8815bb09023cd36993f1439430ad1068c0d5837
todd <todd@jloop.com>
2012-07-02 17:47:52 +0000

    ror twilio

--- ror/app/Controller/TwilioController.php
+++ ror/app/Controller/TwilioController.php
@@ -3,6 +3,10 @@ class TwilioController extends AppController {
 
 	public $name = "TwilioController";
 	
+	public function beforeFilter() {
+		$this->Auth->allow("*");
+	}
+	
 	public function index() {
 		echo 123;
 		die;
