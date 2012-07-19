bbb38d2c93dc15672615b6011078e20ad2b8ea17
michael@jloop.com <michael@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2012-07-19 22:01:33 +0000

    TexMex

--- trunk/ror/app/Controller/QuestionsController.php
+++ trunk/ror/app/Controller/QuestionsController.php
@@ -80,7 +80,7 @@ class QuestionsController extends AppController {
 			//$request = new RestRequest('http://admin:MyAdminPass87@50.56.194.198:8282/RingorangWebService/rservice/game/addQuestions/192', 'POST', $data);
 			//$request->execute();
 			//$response = $request->getResponseBody();
-		}
+		endforeach;
 		exit;
 	}
 	
