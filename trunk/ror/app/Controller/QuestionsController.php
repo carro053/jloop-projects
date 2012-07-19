2c85e41a4b1a3ece520a73cfd4fddbb035d2811b
michael@jloop.com <michael@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2012-07-19 23:54:52 +0000

    TexMex

--- trunk/ror/app/Controller/QuestionsController.php
+++ trunk/ror/app/Controller/QuestionsController.php
@@ -81,7 +81,8 @@ class QuestionsController extends AppController {
 			}
 			$data['state'] = 'Draft';
 			$data['gameId'] = 231;
-			$xml = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><question><answer>'.$data['answer'].'</answer><answer1>'.$data['answer1_en_us'].'</answer1><answer2>'.$data['answer2_en_us'].'</answer2><answer3>'.$data['answer3_en_us'].'</answer3><answer4>'.$data['answer4_en_us'].'</answer4><approved>false</approved><clueText>'.$data['clueText_en_us'].'</clueText><gameId>'.$data['gameId'].'</gameId><insightText>'.$data['insightText_en_us'].'</insightText><langs>en_us</langs><question>'.$data['question_en_us'].'</question><state>'.$data['state'].'</state><type>'.$data['type'].'</type></question>';
+			/*<?xml version="1.0" encoding="UTF-8" standalone="yes"?>*/
+			$xml = '<question><answer>'.$data['answer'].'</answer><answer1>'.$data['answer1_en_us'].'</answer1><answer2>'.$data['answer2_en_us'].'</answer2><answer3>'.$data['answer3_en_us'].'</answer3><answer4>'.$data['answer4_en_us'].'</answer4><approved>false</approved><clueText>'.$data['clueText_en_us'].'</clueText><gameId>'.$data['gameId'].'</gameId><insightText>'.$data['insightText_en_us'].'</insightText><langs>en_us</langs><question>'.$data['question_en_us'].'</question><state>'.$data['state'].'</state><type>'.$data['type'].'</type></question>';
 			//pr($data);
 			echo $xml;
 			echo '<hr>';
