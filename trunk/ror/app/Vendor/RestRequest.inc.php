9e4913fb411ecb462027bd1e062a97cb79455958
michael@jloop.com <michael@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2012-07-20 00:00:36 +0000

    TexMex

--- trunk/ror/app/Vendor/RestRequest.inc.php
+++ trunk/ror/app/Vendor/RestRequest.inc.php
@@ -151,7 +151,7 @@ class RestRequest
 		curl_setopt($curlHandle, CURLOPT_TIMEOUT, 10);
 		curl_setopt($curlHandle, CURLOPT_URL, $this->url);
 		curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array ('Accept: ' . $this->acceptType,"Content-type: application/xml;charset=utf-8"));
+		curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array ('Accept: ' . $this->acceptType,"Content-type: application/xml"));
 	}
 	
 	protected function setAuth (&$curlHandle)
