977e3689087642ec29080b2003451ab83328f4ad
chris <chris@jloop.com>
2013-07-09 21:31:39 +0000

    google PHP API

--- appstore/app/Controller/SearchesController.php
+++ appstore/app/Controller/SearchesController.php
@@ -21,7 +21,7 @@ class SearchesController extends AppController {
 		$search = new Google_CustomsearchService($client);
 		
 		// Example executing a search with your custom search id.
-		$result = $search->cse->listCse('site%3Aitunes.apple.com%2Fus+-%22This+app+is+optimized+for+iPhone+5.%22+%22Open+iTunes+to+buy+and+download+apps.%22+-%22Compatible+with+iPad.%22&rlz=1C5CHFA_enUS503US503&oq=site%3Aitunes.apple.com%2Fus+-%22This+app+is+optimized+for+iPhone+5.%22+%22Open+iTunes+to+buy+and+download+apps.%22+-%22Compatible+with+iPad.%22&aqs=chrome.0.57j58.490j0&sourceid=chrome&ie=UTF-8#q=site:itunes.apple.com/us+-%22This+app+is+optimized+for+iPhone+5.%22+%22Open+iTunes+to+buy+and+download+apps.%22+-%22Compatible+with+iPad.%22', array(
+		$result = $search->cse->listCse('site:itunes.apple.com/us -"This app is optimized for iPhone 5." "Open iTunes to buy and download apps." -"Compatible with iPad."', array(
 		  'cx' => '007301418745006324333:d--m5x9_aui', // The custom search engine ID to scope this search query.
 		));
 		print "<pre>" . print_r($result, true) . "</pre>";
