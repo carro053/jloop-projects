5d0d65385cc1a02abbf12819397a339423026692
chris@jloop.com <chris@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2013-07-09 21:23:00 +0000

    google PHP API

--- trunk/appstore/app/Controller/SearchesController.php
+++ trunk/appstore/app/Controller/SearchesController.php
@@ -24,6 +24,7 @@ class SearchController extends AppController {
 		  'cx' => '007301418745006324333:d--m5x9_aui', // The custom search engine ID to scope this search query.
 		));
 		print "<pre>" . print_r($result, true) . "</pre>";
+		exit;
 		
 		/*
 		// Example executing a search with the URL of a linked custom search engine.
