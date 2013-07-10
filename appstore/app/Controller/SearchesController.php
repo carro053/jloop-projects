faf85353655b8d036c244b64be911cb685e9313d
chris <chris@jloop.com>
2013-07-10 00:02:59 +0000

    google api fun

--- appstore/app/Controller/SearchesController.php
+++ appstore/app/Controller/SearchesController.php
@@ -64,8 +64,8 @@ class SearchesController extends AppController {
 			
 			$result = $search->cse->listCse($query, array(
 			  'cx' => '007301418745006324333:d--m5x9_aui', // The custom search engine ID to scope this search query.
-			  'lowRange' => "100",
-			  'highRange' => "200"
+			  'lowRange' => "0",
+			  'highRange' => "100"
 			));
 			print "<pre>" . print_r($result, true) . "</pre>";
 			exit;
