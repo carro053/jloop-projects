974a4bf38659cc5358c08b6b4163bf3d03aa41e7
chris@jloop.com <chris@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2013-07-19 17:18:12 +0000

    leads index

--- trunk/leads/app/Controller/LeadsController.php
+++ trunk/leads/app/Controller/LeadsController.php
@@ -3,6 +3,31 @@ App::uses('AppController', 'Controller');
 
 class LeadsController extends AppController {
 
+	public function index() {
+		$limit = (!empty($_GET['limit']) ? $_GET['limit'] : 50);
+		$page = (!empty($_GET['page']) ? $_GET['page'] : 1);
+		
+		$conditions = array();
+		$conditions[] = 'Lead.status > 0';
+		
+		//search conditions
+		/*
+		if(!empty($_GET['category'])) {
+			$conditions['Scrape.category'] = $_GET['category'];
+		}
+		*/
+		
+		$leads = $this->Lead->find('all', array(
+			'conditions' => $conditions,
+			'page' => $page,
+			'limit' => $limit
+		));
+		$this->set('leads', $leads);
+		
+		$count = $this->Lead->find('count', array('conditions' => $conditions));
+		$this->set('count', $count);
+	}
+
 	public function gather() {
 	}
 	
