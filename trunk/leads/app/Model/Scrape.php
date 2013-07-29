285b42938dd8460f67c2f82da892f21130e157dc
chris@jloop.com <chris@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2013-07-29 22:45:08 +0000

    chrome extension!

--- trunk/leads/app/Model/Scrape.php
+++ trunk/leads/app/Model/Scrape.php
@@ -26,10 +26,11 @@ class Scrape extends AppModel {
 		//reformat a google search result link
 		if(strpos($url, 'https://www.google.com/url?') !== false) {
 			$parsed_url = parse_url($url);
-			pr($parsed_url);
+			//pr($parsed_url);
 			parse_str($parsed_url['query'], $output);
-			pr($output);
-			exit;
+			//pr($output);
+			$url = $output['url'];
+			//exit;
 		}
 		
 		$html = file_get_contents($url);
