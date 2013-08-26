ea5e970603a9aef41f967f2fd5b0381d5be3749f
todd@jloop.com <todd@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2013-08-26 20:20:23 +0000

    contacts

--- trunk/leads/app/Model/Note.php
+++ trunk/leads/app/Model/Note.php
@@ -4,4 +4,14 @@ App::uses('AppModel', 'Model');
 class Note extends AppModel {
 
 	public $belongsTo = array('User');
+	
+	public $validate = array(
+		'text' => array(
+			array(
+				'rule' => 'notEmpty',
+				'required' => false
+			)
+		)
+	);
+	
 }
