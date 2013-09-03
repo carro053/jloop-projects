dadf8078fc50bc4b8bf06623ba48964cd6f5dc35
todd <todd@jloop.com>
2013-09-03 17:04:56 +0000

    highrise

--- leads/app/Vendor/highrise.php
+++ leads/app/Vendor/highrise.php
@@ -19,74 +19,104 @@ class Highrise {
 		return $parsed_resp;
 	}
 	
-	public function pushPerson($data) {
+	public function pushPerson($contact, $company) {
 		$xml = '<person>
-		  <first-name>John</first-name>
-		  <last-name>Doe</last-name>
-		  <title>CEO</title>
-		  <company-name>API Test Company</company-name>
-		  <background>A popular guy for random data</background>
-		  <linkedin_url>http://us.linkedin.com/in/john-doe</linkedin_url>
+		  <first-name>'.htmlspecialchars($contact['first_name']).'</first-name>
+		  <last-name>'.htmlspecialchars($contact['last_name']).'</last-name>
+		  <title>'.htmlspecialchars($contact['title']).'</title>
+		  <company-name>'.htmlspecialchars($company).'</company-name>
+		  <background>'.htmlspecialchars($contact['background_info']).'</background>
+		  <linkedin_url>'.htmlspecialchars($contact['linkedin']).'</linkedin_url>
 		  <contact-data>
 		    <email-addresses>
 		      <email-address>
-		        <address>john.doe@example.com</address>
+		        <address>'.htmlspecialchars($contact['email']).'</address>
 		        <location>Work</location>
 		      </email-address>
 		    </email-addresses>
 		    <phone-numbers>
 		      <phone-number>
-		        <number>555-555-5555</number>
+		        <number>'.htmlspecialchars($contact['phone']).'</number>
 		        <location>Work</location>
 		      </phone-number>
-		      <phone-number>
-		        <number>555-666-6666</number>
-		        <location>Home</location>
-		      </phone-number>
 		    </phone-numbers>
+		    <addresses>
+			    <address>
+			      <city>'.htmlspecialchars($contact['city']).'</city>
+			      <country>'.htmlspecialchars($contact['country']).'</country>
+			      <state>'.htmlspecialchars($contact['state']).'</state>
+			      <street>'.htmlspecialchars($contact['address']).'</street>
+			      <zip>'.htmlspecialchars($contact['zip']).'</zip>
+			      <location>Work</location>
+			    </address>
+			  </addresses>
+			  <twitter-accounts>
+			    <twitter-account>
+			      <username>'.htmlspecialchars($contact['twitter']).'</username>
+			    </twitter-account>
+			  </twitter-accounts>
+			  <instant-messengers>
+			    <instant-messenger>
+			      <address>'.htmlspecialchars($contact['im']).'</address>
+			      <protocol>AIM</protocol>
+			      <location>Work</location>
+			    </instant-messenger>
+			  </instant-messengers>
+			  <web-addresses>
+			    <web-address>
+			      <url>'.htmlspecialchars($contact['website']).'</url>
+			      <location>Work</location>
+			    </web-address>
+			  </web-addresses>
 		  </contact-data>
-		  <!-- start of custom fields -->
-		  <subject_datas type="array">
-		    <subject_data>
-		      <value>Chicago</value>
-		      <subject_field_id type="integer">2</subject_field_id>
-		    </subject_data>
-		  </subject_datas>
-		  <!-- end of custom fields -->
 		</person>';
 		$resp = $this->post($xml, 'people.xml');
 		return $resp;
 	}
 	
-	public function pushCompany() {
+	public function pushCompany($lead) { //https://github.com/37signals/highrise-api/blob/master/sections/data_reference.md#company
 		$xml = '<company>
-		  <name>Doe Inc.</name>
-		  <background>A popular company for random data</background>
-		  <visible-to>Owner</visible-to>
+		  <name>'.htmlspecialchars($lead['company']).'</name>
+		  <background></background>
+		  <visible-to>Everyone</visible-to>
 		  <contact-data>
 		    <email-addresses>
 		      <email-address>
-		        <address>corporate@example.com</address>
+		        <address>'.htmlspecialchars($lead['email']).'</address>
 		        <location>Work</location>
 		      </email-address>
 		    </email-addresses>
 		    <phone-numbers>
 		      <phone-number>
-		        <number>555-555-5555</number>
+		        <number>'.htmlspecialchars($lead['phone']).'</number>
 		        <location>Work</location>
 		      </phone-number>
-		      <phone-number>
-		        <number>555-666-6667</number>
-		        <location>Fax</location>
-		      </phone-number>
 		    </phone-numbers>
+		    <addresses>
+			    <address>
+			      <city>'.htmlspecialchars($lead['city']).'</city>
+			      <country>'.htmlspecialchars($lead['country']).'</country>
+			      <state>'.htmlspecialchars($lead['state']).'</state>
+			      <street>'.htmlspecialchars($lead['address']).'</street>
+			      <zip>'.htmlspecialchars($lead['zip']).'</zip>
+			      <location>Work</location>
+			    </address>
+			  </addresses>
+			  <twitter-accounts>
+			    <twitter-account>
+			      <username>'.htmlspecialchars($lead['twitter']).'</username>
+			    </twitter-account>
+			  </twitter-accounts>
 		  </contact-data>
-		  <!-- custom fields -->
 		  <subject_datas type="array">
-		    <subject_data>
-		      <value>Chicago</value>
-		      <subject_field_id type="integer">2</subject_field_id>
-		    </subject_data>
+			  <subject_data>
+			    <value>'.htmlspecialchars($lead['facebook']).'</value>
+			    <subject_field_label>Facebook</subject_field_label>
+			  </subject_data>
+			  <subject_data>
+			    <value>'.htmlspecialchars($lead['linkedin']).'</value>
+			    <subject_field_label>Linkedin</subject_field_label>
+			  </subject_data>
 		  </subject_datas>
 		</company>';
 		$resp = $this->post($xml, 'companies.xml');
