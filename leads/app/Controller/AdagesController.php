7c4ebd2c2f69b37098ca08bcd23d556db0945320
chris <chris@jloop.com>
2014-02-12 22:09:20 +0000

    adage scraping

--- leads/app/Controller/AdagesController.php
+++ leads/app/Controller/AdagesController.php
@@ -8,7 +8,8 @@ class AdagesController extends AppController {
 		//exit;
 	
 		App::import('Vendor', 'phpQuery/phpQuery');
-				
+		
+		//Adage uses AJAX to load the actual company names, so you'll have to save the rendered page in your browser and upload it to the following location
 		$html = file_get_contents('http://'.$_SERVER['HTTP_HOST'].'/files/adage_directory.html');
 		
 		//echo $html;
@@ -22,11 +23,21 @@ class AdagesController extends AppController {
 		//pr(pq('a.directory_entry'));
 		
 		foreach(pq('a.directory_entry') as $key => $a) {
+			
 			echo pq($a)->text().'<hr>';
 			echo pq($a)->attr('href').'<hr>';
+			
+			$this->Adage->create();
+			$adage = array();
+			$adage['Adage']['name'] = pq($a)->text();
+			$adage['Adage']['url'] = pq($a)->attr('href');
+			$this->Adage->save($adage, false);
 		}
 		
 		exit;
 	}
 
+	public function scrapeAdage($id) {
+		
+	}
 }
