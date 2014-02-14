80d1ce061955fcd9a655f7485359770fe04299f8
chris@jloop.com <chris@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2014-02-14 18:21:37 +0000

    adage import

--- trunk/leads/app/Model/Adage.php
+++ trunk/leads/app/Model/Adage.php
@@ -5,4 +5,18 @@ class Adage extends AppModel {
 
 	public $belongsTo = array('Lead');
 	
+	public function afterSave($created) {
+		if($created) {
+			$this->Lead->create();
+			$lead = array(
+				'model' => $this->name,
+				'model_id' => $this->id,
+				'type' => $this->data['Lead']['type']
+			);
+			if(!$this->Lead->save($lead, false))
+				die('An error has occurred while saving the lead for '.$this->name.', id: '.$this->id);
+			if(!$this->saveField('lead_id', $this->Lead->id))
+				die('An error has occurred while saving the lead_id for '.$this->name.', id: '.$this->id.', lead_id: '.$this->Lead->id);
+		}
+	}
 }
