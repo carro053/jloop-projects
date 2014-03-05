3595b3ea957cd6d81d40a54cd8cdd268546cb183
chris@jloop.com <chris@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2014-03-05 18:26:43 +0000

    making lead form AJAX'ified

--- trunk/leads/app/Controller/LeadsController.php
+++ trunk/leads/app/Controller/LeadsController.php
@@ -145,6 +145,10 @@ class LeadsController extends AppController {
 					$lead = $this->Lead->findById($this->request->data['Lead']['id']);
 					$this->set('lead', $lead);
 					$tags = $this->Lead->Tag->find('all');
+					
+					pr($tags);
+					exit;
+					
 					$this->set('tags', $tags);
 					return $this->render('/Elements/lead_form');
 				}
