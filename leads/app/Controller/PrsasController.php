064c7aab48c5cf345ff830ebf36e56bd9d92a67c
chris <chris@jloop.com>
2014-04-30 17:03:25 +0000

    prsa scraper

---
+++ leads/app/Controller/PrsasController.php
@@ -0,0 +1,40 @@
+<?php
+App::uses('AppController', 'Controller');
+
+class PrsasController extends AppController {
+
+	//example URLs
+	//http://www.prsa.org/Network/FindAFirm/Search?StartDisplay=1&xName=&xCompany=&xCity=&xIndSpec=&xState=CA&xZip=&xCountry=&xDesignation=&xPracSpec=&xSearchOutput=IND
+	//http://www.prsa.org/Network/FindAFirm/Search?StartDisplay=11&xName=&xCompany=&xCity=&xIndSpec=&xState=CA&xZip=&xCountry=&xDesignation=&xPracSpec=&xSearchOutput=IND
+	//http://www.prsa.org/Network/FindAFirm/Search?StartDisplay=51&xName=&xCompany=&xCity=&xIndSpec=&xState=CA&xZip=&xCountry=&xDesignation=&xPracSpec=&xSearchOutput=IND
+	
+	public function scrapeIndex() {
+		echo '<pre>';
+	
+		App::import('Vendor', 'phpQuery/phpQuery');
+		
+		$has_more_pages = true;
+		$i = 1;
+		$increment = 10;
+		while($has_more_pages) {
+			$url = 'http://www.prsa.org/Network/FindAFirm/Search?StartDisplay='.$i.'&xName=&xCompany=&xCity=&xIndSpec=&xState=CA&xZip=&xCountry=&xDesignation=&xPracSpec=&xSearchOutput=IND';
+			$html = file_get_contents($url);
+			if($html) {
+				//There were 57 matches
+				$total_matches = preg_match('There were [.0-9]+ matches');
+				echo 'total matches: '.$total_matches;
+			
+				$doc = phpQuery::newDocumentHTML($html);
+				
+				
+				
+			} else {
+				echo 'Error fetching: '.$url.'<br>';
+			}
+			if($i > 5000) //JUST IN CASE WE GO TOO FAR!!!
+				$has_more_pages = false;
+			$i += $increment;
+		}
+		exit;
+	}
+}
