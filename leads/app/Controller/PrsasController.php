41974233f88c788c7034f270b20b8bc4cac1c92c
chris <chris@jloop.com>
2014-05-01 20:59:13 +0000

    prsa scraping

--- leads/app/Controller/PrsasController.php
+++ leads/app/Controller/PrsasController.php
@@ -120,7 +120,7 @@ class PrsasController extends AppController {
 			} else {
 				echo 'Error fetching: '.$url.'<br>';
 			}
-			if($i > 500) //JUST IN CASE WE GO TOO FAR!!!
+			if($i > 20) //JUST IN CASE WE GO TOO FAR!!!
 				break;
 			$i += $increment;
 		}
