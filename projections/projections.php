97fc8877999ec402d32b6df0ae8158bc888ef18d
jay <jay@jloop.com>
2014-06-06 05:37:13 +0000

    testing out xero integration

--- projections/projections.php
+++ projections/projections.php
@@ -9,6 +9,22 @@ include("projections_stuff.php");
 
 ////
 
+echo 'month of june<br/><br/>';
+
+echo 'invoices to date:<br />';
+$response = $XeroOAuth->request('GET', $XeroOAuth->url('Invoices', 'core'), array('Where' => 'Status=="DRAFT" && Date>=DateTime(2014,7,1) && Date<DateTime(2014,8,1)'));
+if ($XeroOAuth->response['code'] == 200) {
+	$accounts = $XeroOAuth->parseResponse($XeroOAuth->response['response'], $XeroOAuth->response['format']);
+	echo "There are " . count($accounts->Invoices[0]). " to date </br>";
+	//pr($accounts->Invoices[0]->Invoice);
+	foreach Invoices as Invoice {
+		pr(Invoice);
+	}
+	
+} else {
+	outputError($XeroOAuth);
+}
+
 $response = $XeroOAuth->request('GET', $XeroOAuth->url('RepeatingInvoices', 'core'), array('Where' => 'Schedule.NextScheduledDate>=DateTime(2014,7,1) && Schedule.NextScheduledDate<DateTime(2014,8,1)'));
 if ($XeroOAuth->response['code'] == 200) {
 	$accounts = $XeroOAuth->parseResponse($XeroOAuth->response['response'], $XeroOAuth->response['format']);
