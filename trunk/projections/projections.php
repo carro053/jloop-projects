455c4488315fe4d52e2fe122db775f9aa28f340c
jay@jloop.com <jay@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2014-06-06 05:11:19 +0000

    testing out xero integration

---
+++ trunk/projections/projections.php
@@ -0,0 +1,66 @@
+<?php
+require 'lib/XeroOAuth.php';
+
+define ( 'BASE_PATH', dirname(__FILE__) );
+define ( "XRO_APP_TYPE", "Private" );
+define ( "OAUTH_CALLBACK", "oob" );
+$useragent = "XeroOAuth-PHP Private App Test";
+
+$signatures = array (
+		'consumer_key' => 'VKVCKW3NUXKEPORW7ZWOHBIB7DYTCL',
+		'shared_secret' => 'E2T16UNAVRADG6BQHRIXPCMQKEXXSU',
+		// API versions
+		'core_version' => '2.0',
+		'payroll_version' => '1.0' 
+);
+
+if (XRO_APP_TYPE == "Private" || XRO_APP_TYPE == "Partner") {
+	$signatures ['rsa_private_key'] = BASE_PATH . '/certs/privatekey.pem';
+	$signatures ['rsa_public_key'] = BASE_PATH . '/certs/publickey.cer';
+}
+
+$XeroOAuth = new XeroOAuth ( array_merge ( array (
+		'application_type' => XRO_APP_TYPE,
+		'oauth_callback' => OAUTH_CALLBACK,
+		'user_agent' => $useragent 
+), $signatures ) );
+
+
+$initialCheck = $XeroOAuth->diagnostics ();
+$checkErrors = count ( $initialCheck );
+if ($checkErrors > 0) {
+	// you could handle any config errors here, or keep on truckin if you like to live dangerously
+	foreach ( $initialCheck as $check ) {
+		echo 'Error: ' . $check . PHP_EOL;
+	}
+} else {
+	$session = persistSession ( array (
+			'oauth_token' => $XeroOAuth->config ['consumer_key'],
+			'oauth_token_secret' => $XeroOAuth->config ['shared_secret'],
+			'oauth_session_handle' => '' 
+	) );
+	$oauthSession = retrieveSession ();
+	
+	if (isset ( $oauthSession ['oauth_token'] )) {
+		$XeroOAuth->config ['access_token'] = $oauthSession ['oauth_token'];
+		$XeroOAuth->config ['access_token_secret'] = $oauthSession ['oauth_token_secret'];
+		
+		//include 'tests/tests.php';
+	}
+	
+	//testLinks ();
+}
+
+
+
+
+$response = $XeroOAuth->request('GET', $XeroOAuth->url('RepeatingInvoices', 'core'), array('Where' => 'Schedule.NextScheduledDate>=DateTime(2014,7,1) && Schedule.NextScheduledDate<DateTime(2014,8,1)'));
+if ($XeroOAuth->response['code'] == 200) {
+	$accounts = $XeroOAuth->parseResponse($XeroOAuth->response['response'], $XeroOAuth->response['format']);
+	echo "There are " . count($accounts->RepeatingInvoices[0]). " matching invoices in this Xero organisation, the first one is: </br>";
+	pr($accounts->RepeatingInvoices[0]->RepeatingInvoice);
+} else {
+	outputError($XeroOAuth);
+}
+
+?>
