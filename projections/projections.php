3661a7aa2702145d08ec661fdae897f0dade3101
jay <jay@jloop.com>
2014-06-06 17:47:46 +0000

    testing xero integration

--- projections/projections.php
+++ projections/projections.php
@@ -15,7 +15,7 @@ echo 'invoices to date:<br />';
 $response = $XeroOAuth->request('GET', $XeroOAuth->url('Invoices', 'core'), array('Where' => 'Status=="DRAFT" && Date>=DateTime(2014,7,1) && Date<DateTime(2014,8,1)'));
 if ($XeroOAuth->response['code'] == 200) {
 	$accounts = $XeroOAuth->parseResponse($XeroOAuth->response['response'], $XeroOAuth->response['format']);
-	echo "There are " . count($accounts->Invoices[0]). " to date </br>";
+	echo "There are " . count($accounts->Invoices[0]->Invoice). " to date </br>";
 	//pr($accounts->Invoices[0]->Invoice);
 	pr($accounts->Invoices[0]->Invoice[0]);
 	
