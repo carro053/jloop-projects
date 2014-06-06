2582241d1ddd2f3070af66a70c3b60c65ba50a93
jay <jay@jloop.com>
2014-06-06 18:01:53 +0000

    testing xero integration

--- projections/projections.php
+++ projections/projections.php
@@ -11,16 +11,15 @@ include("projections_stuff.php");
 
 echo 'month of june<br/><br/>';
 
-echo 'invoices to date:<br />';
+echo 'PROJECTED invoices:<br />';
 $response = $XeroOAuth->request('GET', $XeroOAuth->url('Invoices', 'core'), array('Where' => 'Status=="DRAFT" && Date>=DateTime(2014,7,1) && Date<DateTime(2014,8,1)'));
 if ($XeroOAuth->response['code'] == 200) {
 	$accounts = $XeroOAuth->parseResponse($XeroOAuth->response['response'], $XeroOAuth->response['format']);
-	echo "There are " . count($accounts->Invoices[0]->Invoice). " to date </br>";
+	echo "There are " . count($accounts->Invoices[0]->Invoice). " to date <br/>";
 	//pr($accounts->Invoices[0]->Invoice);
 	//pr($accounts->Invoices[0]->Invoice[0]);
 	foreach($accounts->Invoices[0]->Invoice as $inv) {
-		echo 'new';
-		pr($inv);
+		echo $inv->Contact->Name.": ".$inv->Reference." - ".$inv->AmountDue."<br/>";
 	}
 	
 	
