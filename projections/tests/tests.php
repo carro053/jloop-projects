9d26fe27e5053073a6b45d8b7a460471239022cd
jay <jay@jloop.com>
2014-06-06 02:44:53 +0000

    testing out xero integration

--- projections/tests/tests.php
+++ projections/tests/tests.php
@@ -332,6 +332,17 @@ if (isset($_REQUEST['invoicesmodified'])) {
            outputError($XeroOAuth);
        }
    }
+   ////JAY STUFF
+   if (isset($_REQUEST['invoicesdraft'])) {
+       $response = $XeroOAuth->request('GET', $XeroOAuth->url('Invoices', 'core'), array('Status' => 'Draft'));
+       if ($XeroOAuth->response['code'] == 200) {
+           $accounts = $XeroOAuth->parseResponse($XeroOAuth->response['response'], $XeroOAuth->response['format']);
+           echo "There are " . count($accounts->Invoices[0]). " matching invoices in this Xero organisation, the first one is: </br>";
+           pr($accounts->Invoices[0]->Invoice);
+       } else {
+           outputError($XeroOAuth);
+       }
+   }
    
 
 }
