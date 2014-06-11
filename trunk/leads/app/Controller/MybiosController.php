e4f1b841a91fadf3f4c0da39f7bb8529aac60b3b
chris@jloop.com <chris@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2014-06-11 22:26:04 +0000

    myBIO scraping

--- trunk/leads/app/Controller/MybiosController.php
+++ trunk/leads/app/Controller/MybiosController.php
@@ -14,7 +14,7 @@ class MybiosController extends AppController {
 		App::import('Vendor', 'phpQuery/phpQuery');
 		
 		$base_url = 'http://mybio.org/exhibitor?exhibitor_page=';
-		for($i = 11; $i <= 20; $i++) {
+		for($i = 21; $i <= 30; $i++) {
 			echo 'page '.$i,'<br><br>';
 			$html = file_get_contents($base_url.$i);
 			$doc = phpQuery::newDocumentHTML($html);
