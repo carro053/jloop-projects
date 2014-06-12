3c7d74f987c87a8da55895b8c5160a52b34ee7b1
jay@jloop.com <jay@jloop.com@e75b8f88-3201-0010-8c6c-15735ff18bc4>
2014-06-12 00:55:41 +0000

    harvest API

--- trunk/projections/harvest1/harvest_api_sample.php
+++ trunk/projections/harvest1/harvest_api_sample.php
@@ -54,7 +54,7 @@
 			        	foreach ($time_xml as $entry) {
 			        		//if ($entry->is-billed) echo "BILLED: ";
 			        		//else echo "NOT BILLED: "
-			        		echo $entry->is-billed.__toString;
+			        		echo (string) $entry->is-billed;
 				        	echo "Entry: ".strval($entry->notes)."<br />";
 			        	}
 			        	//break;
