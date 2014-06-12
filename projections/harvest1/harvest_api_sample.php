25974468caff8693d2b9d920d7a433f5f96d367c
jay <jay@jloop.com>
2014-06-12 00:49:03 +0000

    harvest API

--- projections/harvest1/harvest_api_sample.php
+++ projections/harvest1/harvest_api_sample.php
@@ -49,7 +49,7 @@
 			            print "Error: " . curl_error($ch);
 			        } else {
 			        	$time_xml = new SimpleXMLElement($data2);
-			        	//print_r($time_xml);
+			        	print_r($time_xml);
 			        	echo "count: ".count($time_xml);
 			        	foreach ($time_xml as $entry) {
 				        	echo "Entry: ".strval($entry->notes)."<br />";
