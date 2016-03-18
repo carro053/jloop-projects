3facd6fdd8532516724f01eab1f3375372bf4cb3
jay <jay@jloop.com>
2016-03-18 17:17:24 +0000

    chargify testing

--- projections/chargify-test.php
+++ projections/chargify-test.php
@@ -3,12 +3,12 @@
 
 require_once('Chargify-PHP-Client/lib/Chargify.php');
 $test = FALSE;
-/*
+
 $customer = new ChargifyCustomer(NULL, $test);
 $customers = $customer->getAllCustomers();
 echo '<h2>Array of 50 customer objects</h2>';
 print_r($customers);
-*/
+
 $subscription = new ChargifySubscription(NULL, $test);
 $subscriptions = $subscription->getAllSubscriptions();
 print_r($subscriptions);
