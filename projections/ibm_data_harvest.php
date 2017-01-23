c302a296ddc894a4f838c59f4b83904900997d48
jay <jay@jloop.com>
2017-01-23 22:37:34 +0000

    ibm data

--- projections/ibm_data_harvest.php
+++ projections/ibm_data_harvest.php
@@ -1,5 +1,5 @@
 <?php
-	
+	$year = "2016";
 	
 	ini_set('display_errors',1); 
 	error_reporting(E_ALL);
@@ -9,7 +9,7 @@
 	//$xml_data = "<request> <notes>qwer</notes> <hours>0.25</hours> <project_id>75406</project_id> <task_id>93182</task_id> <spent_at>Fri, 08 Feb 2008</spent_at> </request>";
 	//$url = "https://jloop.harvestapp.com/projects?updated_since=2014-".$mo."-01+18%3A30";
 	//$url = "https://jloop.harvestapp.com/projects/4380968/entries?from=20140609&to=20140615";
-	$url = "https://jloop.harvestapp.com/projects?client=3381986&updated_since=2016-01-01";
+	$url = "https://jloop.harvestapp.com/projects?client=3381986&updated_since=".$year."-01-01";
 	
 	$headers = array(
 		"Content-type: application/xml",
@@ -40,6 +40,16 @@
 		
 		foreach ($clients->project as $project) {
 			echo $project->name." - ".$project->budget."<br>";
+			$new_url = "https://jloop.harvestapp.com/projects/".$project->id."/entries?from=".$year."0101&to=20170123";
+			/*curl_setopt($ch, CURLOPT_URL, $new_url);
+			$data2 = curl_exec($ch);
+	
+			if (curl_errno($ch)) {
+				print "Error: " . curl_error($ch);
+			} else {
+				$time = simplexml_load_string($data2);
+			}*/
+			echo $new_url."<br>";
 		}
 	}
 ?>
